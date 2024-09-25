a020192fd77177a048676d39896d4d6918742601
AsePlayer <52973701+AsePlayer@users.noreply.github.com>
2024-09-25 16:19:05 -0700

    Added commands and private messages

--- server.gd
+++ server.gd
@@ -50,8 +50,8 @@ func create_client():
 func _on_peer_connected(id):
 	print("Client with ID %d connected to the server." % id)
 	set_username(id)
-	ui.send_server_message.rpc("Welcome to the server, %s!" % get_username(id))
-
+	ui.send_server_message.rpc("%s Joined the server." % get_username(id))
+	send_private_message(id, "Server", "Welcome to the server, %s!\nYou can chat or type /help for a list of commands." % get_username(id))
 
 # Server: handle when a client disconnects
 func _on_peer_disconnected(id):
@@ -60,28 +60,67 @@ func _on_peer_disconnected(id):
 		ui.send_server_message.rpc("%s disconnected from the server." % get_username(id))
 		player_usernames.erase(id)
 
-
 # Client: handle when the client successfully connects to the server
 func _on_connected_to_server():
-	# Send welcome message to all clients
 	print("Client connected to the server successfully.")
-
 
 # Client: handle when connection to the server fails
 func _on_connection_failed():
 	print("Failed to connect to the server.")
 
+# Server: Handle command requests from clients
+@rpc("any_peer")
+func send_command(id, message:String):
+	if message == "/help":
+		# Send a private message back to the client with help info
+		send_private_message(id, "Server", "Here is a list of commands:\n/help - Show this help message\n/list - Show who's online\n/dm {username} - Send a user a direct message\n/name {username} - Change your name")
+	elif message.contains("/list"):
+		# Send a private message back to the client with list of online users
+		var names:String
+		for player in player_usernames:
+			names += ((get_username(player)) + "\n")
+		send_private_message(id, "Server", "Here's a list of who's online right now:\n%s" % names)
+	elif message.contains("/dm "):
+		# Get the id of the player that is being DM'd and send private message
+		var msg = message.split("/dm ")
+		var recipient_username = message.split(" ")[1]
+		var sent_message = message.split(recipient_username)[1]
+		
+		var recipient_id = player_usernames.find_key(recipient_username)
+		# See if user exists.
+		if recipient_id != null:
+			send_private_message(player_usernames.find_key(recipient_username), get_username(id), sent_message)
+		else: # User doesn't exist. DM error to client
+			send_private_message(id, "Server", "Username doesn't exist!\nTry using /list for a list of available names.")
+	elif message.contains("/name "):
+		var name = message.split("/name ")
+		set_username(id, name[1])
+		send_private_message(id, "Server", "Name successfuly changed to: %s" % get_username(id))
+	else:
+		# Broadcast the message as usual
+		send_chat_message(id, message)
 
+
+# Server: Send a message directly to a specific client (private)
+func send_private_message(client_id, from_user, message):
+	# Only send to the specific client
+	send_private_message_to_client.rpc_id(client_id, from_user, message)
+	print("[DM] %s -> %s:%s" % [from_user, get_username(client_id), message])
+
+# Client: Receive the private message from the server
+@rpc("call_local")
+func send_private_message_to_client(from_user, message):
+	ui.send_message("[DM] " + from_user, message)
+
+# Server: Send a chat message to all clients
 @rpc("any_peer")
 func send_chat_message(id, message):
 	ui.send_message.rpc(get_username(id), message)
-	pass
-	
 
 func get_username(sender_id):
-	return player_usernames[sender_id]
-
+	return player_usernames.get(sender_id, "Unknown")
 
 func set_username(sender_id, username=""):
-	if username == "": username = "Guest#%s" % str(randi_range(1,255))
+	if username == "":
+		username = "Guest#%s" % str(randi_range(1, 255))
 	player_usernames[sender_id] = username
